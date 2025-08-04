16314863c2744594a973aa95b439459a129f6972
SterbenDeus <43690865+SterbenDeus@users.noreply.github.com>
2025-08-04 15:02:40 +0300

    Use Hilt for HomeViewModel

--- app/src/main/java/com/example/socialbatterymanager/features/home/data/HomeRepository.kt
+++ app/src/main/java/com/example/socialbatterymanager/features/home/data/HomeRepository.kt
@@ -1,8 +1,11 @@
 package com.example.socialbatterymanager.features.home.data
 
 import com.example.socialbatterymanager.data.database.AppDatabase
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class HomeRepository(private val database: AppDatabase) {
+@Singleton
+class HomeRepository @Inject constructor(private val database: AppDatabase) {
     suspend fun getActivitiesCountFromDate(fromDate: Long): Int {
         return database.activityDao().getActivitiesCountFromDate(fromDate)
     }
