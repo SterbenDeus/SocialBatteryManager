50108203c901219df632999fac9544ceeb17dbe3
SterbenDeus <43690865+SterbenDeus@users.noreply.github.com>
2025-09-06 15:11:29 +0300

    chore: upgrade Gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,10 @@
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.3")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.5")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.57")
-    }
+    id("com.android.application") version "8.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
 }
